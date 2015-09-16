69dcaca44c314242912358fcbf92d8c91ec2e4ee
Tyler Finley & Brandon Seroyer <student@epicodus.com>
2015-09-16 14:30:21 -0700

    add working specs and roll dice feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
-function pigDice(player1, initialScore1) {
-  this.player1Name = player1;
-  this.initialScore1 = initialScore1;
-  this.balance1 = initialScore1;
-  var depositAmount = (Math.round(Math.random) * 6);
+function Player(name, initialScore, balanceScore) {
+  this.name = name;
+  this.initialScore = 0;
+  this.balanceScore = balanceScore;
+  //
+  // initial score changes with result of dice roll
+  // balance score that updates when a player chooses to hold
+  // balance is never affected by the dice roll
+  // initial score that is put to 0 when the dice === 1
+  //
+  //
+  this.balanceScore = balanceScore + initialScore;
 
 }
 
-pigDice.prototype.deposit = function(depositAmount) {
+Player.prototype.deposit = function() {
+  var depositAmount = (Math.floor(Math.random() * (7 - 1)) +1);
   if (depositAmount === 1) {
-  var balance1 = 0;
-  
-  }
-  else {
-  this.balance1 += depositAmount;
-  }
+  var initialScore = 0;
+  } else {
+  this.initialScore += depositAmount;
 
+}
+
+Player.prototype.hold = function() {
+  var balanceScore = initialScore;
+
+  return balanceScore;
+
+}
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,24 @@
-describe('pigDice', function(){
+describe('Player', function(){
   it("sets a player score to zero", function(){
-    var player1 = new pigDice("brandon", 0);
-    expect(player1.player1Name).to.equal("brandon");
-    expect(player1.initialScore1).to.equal(0);
-    expect(player1.balance1).to.equal(0);
+    var name = new Player("brandon");
+    expect(name.name).to.equal("brandon");
+    expect(name.initialScore).to.equal(0);
+
   });
   it("adds an amount to the player score", function(){
-    var player1 = new pigDice("brandon", 0);
-    player1.deposit(4);
-    expect(player1.balance1).to.equal(4);
+    var name = new Player("brandon");
+    name.deposit();
+    expect(name.initialScore).to.be.lessThan(7);
   });
   it("adds a random amount between range 1-6 to the player score", function(){
-    var player1 = new pigDice("brandon", 4);
-    player1.deposit(3);
-    expect(player1.balance1).to.equal(7);
+    var name = new Player("brandon");
+    name.deposit();
+    expect(name.initialScore).to.be.greaterThan(-1);
   });
-  it("resets a player score to 0 if a 1 is rolled", function(){
-    var player1 = new pigDice("brandon", 7);
-    player1.deposit(1);
-    expect(player1.balance1).to.equal(0);
+  it("holds the players score if they choose to stop rolling", function(){
+    var name = new Player("brandon");
+    name.hold();
+    expect(name.balanceScore).to.be.greaterThan(0);
+
   });
 });
